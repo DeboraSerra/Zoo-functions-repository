c5dc91cbe86f42af547f7d5b91abfcd4242c2964
Débora Serra <debora.r.serra@gmail.com>
2022-01-25 15:14:19 -0300

    Corrige lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,9 @@
 const data = require('../data/zoo_data');
+
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  const newArray = ids.map((id) => {
-    return species.find((specie) => specie.id === id);
-  });
+  const newArray = ids.map((id) => species.find((specie) => specie.id === id));
   return newArray;
 }
 
