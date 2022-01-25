97569f537b9305b297e8fe9f3fec69b2c43bf59c
Débora Serra <debora.r.serra@gmail.com>
2022-01-25 19:24:01 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimalId = employees
+    .find((employee) => employee.id === id).responsibleFor[0];
+  const firstAnimal = species
+    .find((specie) => specie.id === firstAnimalId).residents
+    .sort((a, b) => b.age - a.age);
+  return Object.values(firstAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
