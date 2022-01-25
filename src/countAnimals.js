edf85d8c6a9cfce9d5208143b8a62e26e59c3168
Débora Serra <debora.r.serra@gmail.com>
2022-01-25 16:08:44 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+console.log(species[0]);
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const newObj = {};
+    species.map((specie) => {
+      newObj[specie.name] = specie.residents.length;
+      return newObj;
+    });
+    return newObj;
+  }
+  if (!animal.sex) {
+    const findSpecie = species
+      .find((specie) => specie.name === animal.specie);
+    return findSpecie.residents.length;
+  }
+  const findSpecieBySex = species
+    .find((specie) => specie.name === animal.specie).residents
+    .filter((item) => item.sex === animal.sex);
+  return findSpecieBySex.length;
 }
 
 module.exports = countAnimals;
