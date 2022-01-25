88d9fdcaf58292c286403949744a697275857db2
Débora Serra <debora.r.serra@gmail.com>
2022-01-25 16:35:19 -0300

    Adiciona requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const newObj = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  if (entrants.length > 0) {
+    newObj.child = entrants
+      .filter((item) => item.age < 18).length;
+    newObj.adult = entrants
+      .filter((item) => item.age >= 18 && item.age < 50).length;
+    newObj.senior = entrants
+      .filter((item) => item.age >= 50).length;
+  }
+  return newObj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants === {}) return 0;
+  const entries = countEntrants(entrants);
+  let total = 0;
+  total += entries.child * prices.child;
+  total += entries.adult * prices.adult;
+  total += entries.senior * prices.senior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
