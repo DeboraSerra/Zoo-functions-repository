63a423f285c4deca2edef87fbc3d642d13bfbfb4
Débora Serra <debora.r.serra@gmail.com>
2022-01-25 14:59:37 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = data;
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const newArray = ids.map((id) => {
+    return species.find((specie) => specie.id === id);
+  });
+  return newArray;
 }
 
 module.exports = getSpeciesByIds;
