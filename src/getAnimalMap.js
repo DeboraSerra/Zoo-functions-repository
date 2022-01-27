1fadc26fc18da78e712c603252e8972d5bbf99e3
Débora Serra <debora.r.serra@gmail.com>
2022-01-27 15:45:03 -0300

    Refatora requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,101 +2,84 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-function sortedAnimalsBySex(sex) {
+const createObject = (callback) => {
   const newObj = {};
   species.map((specie) => {
     if (!newObj[specie.location]) {
-      newObj[specie.location] = [{
-        [specie.name]: specie.residents
-          .filter((item) => item.sex === sex)
-          .map((animal) => animal.name).sort(),
-      }];
-    } else {
-      newObj[specie.location].push({
-        [specie.name]: specie.residents
-          .filter((item) => item.sex === sex)
-          .map((animal) => animal.name).sort(),
-      });
+      newObj[specie.location] = callback(specie.location);
+      return newObj;
     }
     return true;
   });
   return newObj;
-}
+};
 
-function mapOfAll() {
-  const newObj = {};
-  species.map((specie) => {
-    if (!newObj[specie.location]) {
-      newObj[specie.location] = [specie.name];
-    } else {
-      newObj[specie.location].push(specie.name);
-    }
-    return true;
-  });
-  return newObj;
-}
+const animalsByLocation = (location) =>
+  species.filter((item) => item.location === location);
 
-function animalsWithNames() {
-  const newObj = {};
-  species.map((specie) => {
-    if (!newObj[specie.location]) {
-      newObj[specie.location] = [{
-        [specie.name]: specie.residents.map((item) => item.name),
-      }];
-    } else {
-      newObj[specie.location].push({
-        [specie.name]: specie.residents.map((item) => item.name),
-      });
-    }
-    return true;
-  });
-  return newObj;
-}
+const whichAnimalsByLocation = (location) =>
+  animalsByLocation(location).map((animal) => animal.name);
 
-function sortedAnimalsWithName(options) {
-  if (options.sex) return sortedAnimalsBySex(options.sex);
-  const newObj = {};
-  species.map((specie) => {
-    if (!newObj[specie.location]) {
-      newObj[specie.location] = [{
-        [specie.name]: specie.residents.map((item) => item.name).sort(),
-      }];
-    } else {
-      newObj[specie.location].push({
-        [specie.name]: specie.residents.map((item) => item.name).sort(),
-      });
-    }
-    return true;
-  });
-  return newObj;
-}
+const animalsByLocationName = (location) =>
+  animalsByLocation(location).map((animal) => ({
+    [animal.name]: animal.residents.map((item) => item.name),
+  }));
 
-function animalsBySex(options) {
-  if (options.sorted) return sortedAnimalsBySex(options.sex);
-  const newObj = {};
-  species.map((specie) => {
-    if (!newObj[specie.location]) {
-      newObj[specie.location] = [{
-        [specie.name]: specie.residents
-          .filter((item) => item.sex === options.sex).map((animal) => animal.name),
-      }];
-    } else {
-      newObj[specie.location].push({
-        [specie.name]: specie.residents
-          .filter((item) => item.sex === options.sex).map((animal) => animal.name),
-      });
-    }
-    return true;
-  });
-  return newObj;
-}
+const animalsByLocationNameSort = (location) =>
+  animalsByLocation(location).map((animal) => ({
+    [animal.name]: animal.residents.map((item) => item.name).sort(),
+  }));
+
+const animalsBySex2 = (location, sex) =>
+  animalsByLocation(location).map((item) => ({
+    [item.name]: item.residents
+      .filter((animal) => animal.sex === sex)
+      .map((item2) => item2.name),
+  }));
+
+const animalsBySexSort = (location, sex) =>
+  animalsBySex2(location, sex).map((item) => ({
+    [Object.keys(item)[0]]: item[Object.keys(item)].sort(),
+  }));
+
+const createOtherObject = (sex, sort) => {
+  if (sort) {
+    const newObj = {};
+    species.map((specie) => {
+      newObj[specie.location] = animalsByLocationNameSort(specie.location);
+      return newObj;
+    });
+    return newObj;
+  }
+  if (sex) {
+    const newObj = {};
+    species.map((specie) => {
+      newObj[specie.location] = animalsBySex2(specie.location, sex);
+      return newObj;
+    });
+    return newObj;
+  }
+};
+
+const otherObject = (sex, sort) => {
+  if (sort && sex) {
+    const newObj = {};
+    species.map((specie) => {
+      newObj[specie.location] = animalsBySexSort(specie.location, sex);
+      return newObj;
+    });
+    return newObj;
+  }
+  return createOtherObject(sex, sort);
+};
 
 function getAnimalMap(options) {
-  if (!options || !options.includeNames) return mapOfAll();
+  if (!options || !options.includeNames) {
+    return createObject(whichAnimalsByLocation);
+  }
   const { sex, sorted } = options;
-  if (sorted) return sortedAnimalsWithName(options);
-  if (sex) return animalsBySex(options);
-  return animalsWithNames();
+  if (sorted || sex) return otherObject(sex, sorted);
+  return createObject(animalsByLocationName);
 }
 
 module.exports = getAnimalMap;
